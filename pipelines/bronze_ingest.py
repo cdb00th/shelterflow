0ae06a5d3eab3c71730f2c511ea992518157b56b
cdb00th <charlotte.d.booth@gmail.com>
2026-03-24 17:48:56 -0400

    Implemented bronze_ingest.py

---
+++ pipelines/bronze_ingest.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import duckdb
+
+con = duckdb.connect("../data/shelterflow.duckdb")
+
+# Load CSVs into Pandas DataFrames
+intakes = pd.read_csv("../data/bronze/aac_intakes.csv")
+outcomes = pd.read_csv("../data/bronze/aac_outcomes.csv")
+
+# Create DuckDB tables from Pandas DataFrames
+con.execute("CREATE TABLE bronze_intakes AS SELECT * FROM intakes")
+con.execute("CREATE TABLE bronze_outcomes AS SELECT * FROM outcomes")
+
+con.close()
