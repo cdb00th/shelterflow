aa4b16129fd398b6fbc4c0b644f63f3c3160d8ba
cdb00th <charlotte.d.booth@gmail.com>
2026-03-29 11:44:37 -0400

    fix: overwrite DuckDB tables if they already exist

--- pipelines/bronze_ingest.py
+++ pipelines/bronze_ingest.py
@@ -7,8 +7,8 @@ con = duckdb.connect("../data/shelterflow.duckdb")
 intakes = pd.read_csv("../data/bronze/aac_intakes.csv")
 outcomes = pd.read_csv("../data/bronze/aac_outcomes.csv")
 
-# Create DuckDB tables from Pandas DataFrames
-con.execute("CREATE TABLE bronze_intakes AS SELECT * FROM intakes")
-con.execute("CREATE TABLE bronze_outcomes AS SELECT * FROM outcomes")
+# Create (or replace existing) DuckDB tables from Pandas DataFrames
+con.execute("CREATE OR REPLACE TABLE bronze_intakes AS SELECT * FROM intakes")
+con.execute("CREATE OR REPLACE TABLE bronze_outcomes AS SELECT * FROM outcomes")
 
 con.close()
